fcbda0ffb00d4b13f9d2eaa866314510157087d8
joelsct98 <joelsct98@gmail.com>
2020-11-01 22:41:05 +0100

    Orientar nuestro repositorio a términos del dominio

--- src/main/java/com/platzimarket/persistence/ProductoRepository.java
+++ src/main/java/com/platzimarket/persistence/ProductoRepository.java
@@ -5,6 +5,7 @@ import com.platzimarket.domain.repository.ProductRepository;
 import com.platzimarket.persistence.crud.ProductoCrudRepository;
 import com.platzimarket.persistence.entity.Producto;
 import com.platzimarket.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,8 +15,10 @@ import java.util.Optional;
 
 @Repository
 public class ProductoRepository implements ProductRepository {
-
+    @Autowired
     private ProductoCrudRepository productoCrudRepository;
+
+    @Autowired
     private ProductMapper mapper;
 
     @Override
